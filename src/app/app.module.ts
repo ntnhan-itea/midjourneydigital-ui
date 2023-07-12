75e0b52d03dc9bc1cfe4cd66982a36e2327b355d
ntnhan <nhannguyen039@gmail.com>
2023-07-12 22:56:29 +0700

    Finish migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NeverlandComponent } from './modules/neverland/neverland.component';
@@ -8,13 +13,8 @@ import { DescriptionInfoComponent } from './shared/components/description-info/d
 import { LeftNavComponentComponent } from './shared/components/left-nav-component/left-nav-component.component';
 import { MessagePopupComponent } from './shared/components/message-popup/message-popup.component';
 import { PromptDetailDialogComponent } from './shared/components/prompt-detail-dialog/prompt-detail-dialog.component';
-import { TermsOfUsePopupComponent } from './shared/components/terms-of-use-popup/terms-of-use-popup.component';
 import { RequestFeatureComponent } from './shared/components/request-feature/request-feature.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { TermsOfUsePopupComponent } from './shared/components/terms-of-use-popup/terms-of-use-popup.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +35,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     CollapseModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
